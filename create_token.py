e85ac43d7816a1ded484556ca2c28da0320c60cd
Malik Yusuf <themalikyusuf@gmail.com>
2023-06-13 15:55:41 +0100

    testing

---
+++ create_token.py
@@ -0,0 +1,26 @@
+import requests
+
+def create_github_token(username, password):
+    url = 'https://api.github.com/authorizations'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    data = {
+        'scopes': ['repo'],
+        'note': 'GitHub Token for Automation'
+    }
+    response = requests.post(url, auth=(username, password), headers=headers, json=data)
+    if response.status_code == 201:
+        token = response.json().get('token')
+        print(f'GitHub Token created: {token}')
+        return token
+    else:
+        print(f'Failed to create GitHub Token: {response.json().get("message")}')
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 3:
+        print('Usage: python create_token.py TIKODUDE_USERNAME TIKODUDE_PASSWORD')
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        create_github_token(username, password)
